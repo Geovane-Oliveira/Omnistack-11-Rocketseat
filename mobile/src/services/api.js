56395746a37c8fc2d8d98fe56324eee410db0ed2
Geovane-Oliveira <speedtestf1@gmail.com>
2020-03-26 19:45:29 -0300

    Incidents - index.js corrigido + Expo

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://192.168.1.33:3333'
+	//Para rodar na rede (IP da máquina):
+	//baseURL: 'http://192.168.1.33:3333'
+	
+	//Para rodar no Expo:
+    baseURL: 'http://192.168.1.33:19000'
 });
 
 export default api;
